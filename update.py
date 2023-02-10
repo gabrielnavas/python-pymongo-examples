7234a50cab939724a7aad2e67d18ee5f124a1557
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-02-10 08:08:03 -0300

    update

--- update.py
+++ update.py
@@ -1,11 +1,12 @@
+import pprint
 from connection import client
 
 database_live = client['live']
 pessoas_collection = database_live['pessoas']
 
+# atualizar dados e adicionar dados
 # usar o $set ele apenas atualiza o documento encontrado
 # caso não usar o $set ele atualiza o documento inteiro
-
 pessoas_collection.find_one_and_update(
     { "name": "jucas"},  
     { "$set": { "name": "josefino@@@@@@@@@" }}
